ab840d1df722728ff847b343c77defb543eba38a
luisvilla315 <107729218+luisvilla315@users.noreply.github.com>
2023-01-03 20:42:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { writeFile } = require('../assets/generateMarkdown.js');
+const { writeFile } = require('./assets/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const userPrompt = () => {
